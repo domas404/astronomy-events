800e2165a55992bbc36bebd930b5a750c25f6aa5
domas <domas.guiga@gmail.com>
2025-02-12 12:56:56 +0200

    move home page data fetching to server side

--- app/components/ui/Panel.tsx
+++ app/components/ui/Panel.tsx
@@ -1,15 +1,16 @@
-'use client';
+// 'use client';
 
 import { Asteroid, Comet } from "./Illustrations";
-import { useBodies } from "@/app/hooks/useBodies";
+// import { useBodies } from "@/app/hooks/useBodies";
 import { SBDB_Data } from "@/app/lib/types/SBDB";
 import { HomeDataView } from "../small-body/DataView";
-import { useSelectedBody } from "@/app/hooks/useSelectedBody";
-import { useSelectedData } from "@/app/hooks/useSelectedData";
-import { HomeDataSkeleton } from "../skeletons/Skeletons";
+// import { useSelectedBody } from "@/app/hooks/useSelectedBody";
+// import { useSelectedData } from "@/app/hooks/useSelectedData";
+// import { HomeDataSkeleton } from "../skeletons/Skeletons";
 import Card from "./Card";
-import { useAppSelector } from "@/app/lib/redux/hooks";
-import { headerText, homePanelText } from "@/app/lib/locale-text/ui-text";
+import { fetchClosestBodies, fetchSelectedBody } from "@/app/lib/utils/getNasaData";
+// import { useAppSelector } from "@/app/lib/redux/hooks";
+// import { headerText, homePanelText } from "@/app/lib/locale-text/ui-text";
 
 type DataToDisplay = {
     [key: string]: string | undefined;
@@ -30,17 +31,20 @@ const DataList = ({ data }: { data: SBDB_Data }) => {
     return mappedData;
 }
 
-export default function Panel({ kind }: { kind: 'c' | 'a' }) {
+export default async function Panel({ kind }: { kind: 'c' | 'a' }) {
 
-    const { data: bodiesData, loading: bodiesLoading } = useBodies({ kind, limit: 1 });
-    const { selectedEvent } = useSelectedBody({ data: bodiesData, loading: bodiesLoading });
-    const { data, loading } = useSelectedData({ selectedEvent });
-    const { language } = useAppSelector((state) => state.language);
+    // const { data: bodiesData, loading: bodiesLoading } = useBodies({ kind, limit: 1 });
+    // const { selectedEvent } = useSelectedBody({ data: bodiesData, loading: bodiesLoading });
+    // const { data, loading } = useSelectedData({ selectedEvent });
+
+    const bodiesData = await fetchClosestBodies({ kind, limit: 1 });
+    const data = await fetchSelectedBody({ des: bodiesData.data[0].des });
+    // const { language } = useAppSelector((state) => state.language);
 
     return (
         <div className="px-4 py-6 w-full flex flex-col bg-space-background sm:w-4/5 sm:mx-auto md:w-[70%] lg:w-[80%]
             border-b border-space-border">
-            <div className="text-3xl capitalize">{ kind === 'c' ? headerText[language].comets : headerText[language].asteroids }</div>
+            {/* <div className="text-3xl capitalize">{ kind === 'c' ? headerText[language].comets : headerText[language].asteroids }</div> */}
             <div className="flex flex-row gap-2 mt-4 mb-6">
                 {
                     kind === 'c' ?
@@ -57,14 +61,10 @@ export default function Panel({ kind }: { kind: 'c' | 'a' }) {
             <div className="flex flex-row justify-start items-center h-16">
                 <div className="flex flex-col items-start gap-1">
                     <div className="text-2xl">
-                        { kind === 'c' ? homePanelText[language].nearestComet : homePanelText[language].nearestAsteroid }
+                        {/* { kind === 'c' ? homePanelText[language].nearestComet : homePanelText[language].nearestAsteroid } */}
                     </div>
                     <div className="text-sm md:text-base md:font-semibold font-bold text-space-text-secondary">
-                        {
-                            loading || !data ?
-                            <div className="h-5 bg-space-button-active rounded-full w-32 mt-1 animate-pulse"></div> :
-                            data.object?.fullname
-                        }
+                        {data.object?.fullname}
                     </div>
                 </div>
             </div>
@@ -74,11 +74,7 @@ export default function Panel({ kind }: { kind: 'c' | 'a' }) {
                     { kind === 'a' && <Asteroid /> }
                 </div>
                 <ul className="w-2/3 mx-6 my-4 flex flex-col md:flex-row flex-nowrap md:mx-8 md:flex-wrap md:pl-8 md:border-l border-space-border">
-                    {
-                        loading || !data ?
-                        <HomeDataSkeleton /> :
-                        <DataList data={data} />
-                    }
+                    <DataList data={data} />
                 </ul>
             </div>
             <a href={ kind === 'c' ? './comets' : './asteroids' } className="mt-6 transition-colors hover:cursor-pointer p-3 rounded-md text-sm flex justify-center
