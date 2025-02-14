d7a5df120b347096091be25abf9a134957ed6697
domas <domas.guiga@gmail.com>
2025-02-14 19:12:40 +0200

    add fallback components to home page

--- app/components/skeletons/Skeletons.tsx
+++ app/components/skeletons/Skeletons.tsx
@@ -1,4 +1,6 @@
 import TableHead from "../small-body/TableHead";
+import { Asteroid, Comet, LunarEclipse, SolarEclipse } from "../ui/Illustrations";
+import TextPrimary from "../ui/TextPrimary";
 
 export const TableSkeleton = () => {
     const rowSkeletons = Array(10).fill(null);
@@ -60,4 +62,57 @@ export const HomeDataSkeleton = () => {
         )
     });
     return mappedSkeleton;
+}
+
+export const PanelSkeleton = ({ eventType }: { eventType: string }) => {
+    return (
+        <div className="px-4 py-10 w-full flex flex-col bg-space-background sm:w-4/5 sm:mx-auto md:w-[70%] lg:w-[80%] border-b border-space-border">
+            <TextPrimary eventType={eventType} />
+            <div className="flex flex-row flex-wrap h-[76px] gap-2 mt-4 mb-6 lg:flex-nowrap animate-pulse">
+                <div className={`bg-space-button rounded-lg flex flex-col w-1/2 p-3 lg:w-1/3 lg:max-w-[250px]`}></div>
+            </div>
+            <div className="flex flex-col w-full">
+                <header className="flex flex-col items-start gap-1 h-16 justify-center">
+                    <div className="h-6 bg-space-button-active rounded-full w-32 mt-1 animate-pulse"></div>
+                    <div className="h-4 bg-space-button-active rounded-full w-24 my-1 animate-pulse"></div>
+                </header>
+                <section className="flex flex-row">
+                    <div className="h-52 w-1/3 min-w-[150px] sm:min-w-[180px] bg-black/20 rounded-3xl flex justify-center items-center">
+                        { eventType === 'a' && <Asteroid /> }
+                        { eventType === 'c' && <Comet /> }
+                    </div>
+                    <ul className="w-2/3 mx-6 my-4 flex flex-col flex-nowrap md:mx-8 md:pl-6 md:border-l border-space-border">
+                        <MainDataSkeleton />
+                    </ul>
+                </section>
+            </div>
+            <div className="mt-6 h-11 rounded-md flex justify-center bg-space-button lg:w-64 lg:mx-auto animate-pulse"></div>
+        </div>
+    );
+}
+
+export const EventPanelSkeleton = ({ eventType }: { eventType: string }) => {
+    return (
+        <div className={`px-4 py-10 w-full flex flex-col bg-space-background sm:w-4/5 sm:mx-auto md:w-[70%] lg:w-[80%] border-b border-space-border
+            ${eventType === 'sun' && 'border-b border-space-border'}`}>
+            <TextPrimary eventType={eventType} />
+            <div className="flex flex-row flex-wrap h-[76px] gap-2 mt-4 mb-6 lg:flex-nowrap animate-pulse">
+                <div className={`bg-space-button rounded-lg flex flex-col w-full p-3 lg:w-1/3 lg:max-w-[250px]`}></div>
+            </div>
+            <div className="flex flex-col w-full">
+                <header className="flex flex-col items-start gap-1 h-11 justify-center">
+                    <div className="h-6 bg-space-button-active rounded-full w-32 mt-1 animate-pulse"></div>
+                </header>
+                <section className="flex flex-row">
+                    <div className="h-52 w-1/3 min-w-[150px] sm:min-w-[180px] bg-black/20 rounded-3xl flex justify-center items-center">
+                        { eventType === 'sun' && <SolarEclipse /> }
+                        { eventType === 'moon' && <LunarEclipse /> }
+                    </div>
+                    <ul className="w-2/3 mx-6 my-4 flex flex-col flex-nowrap md:mx-8 md:pl-6 md:border-l border-space-border">
+                        <MainDataSkeleton />
+                    </ul>
+                </section>
+            </div>
+        </div>
+    );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
+import { Suspense } from "react";
 import EventPanel from "./components/large-body/EventPanel";
 import Panel from "./components/small-body/Panel";
 import { Metadata } from "next";
+import { EventPanelSkeleton, PanelSkeleton } from "./components/skeletons/Skeletons";
 
 export const metadata: Metadata = {
 	title: 'Home | Astronomy Events',
@@ -12,10 +14,18 @@ export default function Home() {
 			bg-space-background lg:border-space-border lg:w-[90%] md:max-w-[1200px]">
 			<main className="w-full flex flex-col items-center gap-8">
 				<div className="w-full flex flex-row flex-wrap items-start">
-					<Panel kind="c" />
-					<Panel kind="a" />
-					<EventPanel eventType="sun" />
-					<EventPanel eventType="moon" />
+					<Suspense fallback={<PanelSkeleton eventType={'c'} />}>
+						<Panel kind="c" />
+					</Suspense>
+					<Suspense fallback={<PanelSkeleton eventType={'a'} />}>
+						<Panel kind="a" />
+					</Suspense>
+					<Suspense fallback={<EventPanelSkeleton eventType={'sun'} />}>
+						<EventPanel eventType="sun" />
+					</Suspense>
+					<Suspense fallback={<EventPanelSkeleton eventType={'moon'} />}>
+						<EventPanel eventType="moon" />
+					</Suspense>
 				</div>
 			</main>
 		</div>
